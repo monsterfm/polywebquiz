78aa240c3afd4183668dab70764fa8fd9b8954e6
fafa <fazil-abderaouf.messaoud@polymtl.ca>
2016-10-25 18:22:06 -0400

    question 9

--- TP3/public/javascripts/tabBord.js
+++ TP3/public/javascripts/tabBord.js
@@ -38,6 +38,35 @@ $(function() {
         //localStorage.clear();
         //updateStats();
     }); 
+
+    $('#Details').click(function(){
+        var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
+        if(existingEntries == null) 
+            $('#textDetails').text("Aucun examens n'a encore ete fait!");
+        else{
+            for(i in existingEntries){
+                var numeroExam=parseInt(i)+1;
+                $('#listeExams').append("<li>Examen "+ numeroExam +" - ("+ existingEntries[i].domaine +") : " +existingEntries[i].nbQuestionsCorrectes + "/"
+                    +existingEntries[i].nbQuestionsExamen +"</li><br>");
+            }
+        }
+    }); 
+
+
         
     
 });
+function moyenneExamens(){
+        var existingEntries = JSON.parse(localStorage.getItem("allEntries"));
+        var moyenneExams=0;
+        if(existingEntries == null) 
+            $('#stats').text("Aucun examens n'a encore ete fait!");
+        else{
+            for(i in existingEntries){
+                moyenneExams=moyenneExams+((parseInt(existingEntries[i].nbQuestionsCorrectes)/parseInt(existingEntries[i].nbQuestionsExamen))*100);
+            }
+            moyenneExams=(moyenneExams/existingEntries.length);
+            $('#stats').text("Moyenne des examens: " + moyenneExams +" %");
+        }
+
+    }
