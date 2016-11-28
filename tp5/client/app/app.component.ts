7422745d4b744b0298e62dffec5c3f7cff9f11ed
fafa <fazil-abderaouf.messaoud@polymtl.ca>
2016-11-28 08:42:27 -0500

    brouilon tp5

---
+++ tp5/client/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component  } from '@angular/core';
+
+@Component({
+  selector: 'mon-app',
+  templateUrl: "/templates/acceuil"
+})
+
+export class AppComponent {
+  title = "Tour of heroes";
+  private value = "Hello";
+}
